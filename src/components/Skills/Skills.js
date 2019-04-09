fa7e212f6f048fab0268fba0190abd60878a3065
Gavin Quirk <gavinquirk@gmail.com>
2019-04-09 13:23:31 -0700

    basic styling and responsiveness for skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,82 +6,48 @@ export default class Skills extends Component {
   render() {
     return (
       <div className='Skills'>
-        {/* <div className='introduction-skills'>
-          <div className='skills-section design'>
-            <p className='skills-icon'>ICON</p>
-            <h4 className='skills-heading'>Design</h4>
-            <p className='skills-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
-              nesciunt facilis. Rem obcaecati recusandae sequi vero numquam
-              vitae tempore voluptates.
-            </p>
-            <h5 className='skills-subheading'>What I Design</h5>
-            <p className='skills-languages'>apps, mobile, etc</p>
-            <h5 className='skills-subheading'>My Favorite Tools</h5>
-            <ul className='skills-tools'>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-            </ul>
+        {/* SKILL 1 */}
+        <div className='skill skill-1'>
+          <div className='skill-icon'>
+            <i className='fas fa-pencil-ruler' />
           </div>
-          <div className='skills-section design'>
-            <p className='skills-icon'>ICON</p>
-            <h4 className='skills-heading'>Design</h4>
-            <p className='skills-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
-              nesciunt facilis. Rem obcaecati recusandae sequi vero numquam
-              vitae tempore voluptates.
-            </p>
-            <h5 className='skills-subheading'>What I Design</h5>
-            <p className='skills-languages'>apps, mobile, etc</p>
-            <h5 className='skills-subheading'>My Favorite Tools</h5>
-            <ul className='skills-tools'>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-            </ul>
+          <div className='skill-main-heading'>Design</div>
+          <div className='skill-text'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+            tempore dignissimos pariatur, eos accusantium molestias magni
+            doloribus ducimus! Quo, eius?
           </div>
-          <div className='skills-section design'>
-            <p className='skills-icon'>ICON</p>
-            <h4 className='skills-heading'>Design</h4>
-            <p className='skills-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
-              nesciunt facilis. Rem obcaecati recusandae sequi vero numquam
-              vitae tempore voluptates.
-            </p>
-            <h5 className='skills-subheading'>What I Design</h5>
-            <p className='skills-languages'>apps, mobile, etc</p>
-            <h5 className='skills-subheading'>My Favorite Tools</h5>
-            <ul className='skills-tools'>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-              <li className='li'>tool 1</li>
-            </ul>
+          <div className='skill-subheading'>SUBHEADING</div>
+        </div>
+        {/* SKILL 2 */}
+        <div className='skill skill-2'>
+          <div className='skill-icon'>
+            <i className='fas fa-file-code' />
           </div>
-        </div> */}
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint in
-          fugiat at corporis necessitatibus officia vel aliquam inventore
-          architecto. Maiores quasi explicabo aspernatur tempore inventore
-          maxime sed necessitatibus laudantium nisi reprehenderit ut libero,
-          voluptate quas in ducimus corporis a ad aliquid dignissimos, ullam
-          officiis magnam exercitationem. Officia sed, mollitia illum illo magni
-          voluptatem perspiciatis sequi accusamus saepe atque exercitationem,
-          dicta, autem nihil dolores deleniti voluptates porro quibusdam error.
-          Ipsa quidem recusandae omnis velit ab corporis alias non debitis
-          possimus! Deserunt, aut. Laudantium quod nulla repellendus earum
-          possimus explicabo tempore ut quidem, quis nesciunt, vero rerum
-          distinctio aliquid labore similique quae. Praesentium, quam sint
-          accusantium doloribus impedit adipisci dignissimos aut aliquam quasi
-          sed optio voluptatibus perferendis fugiat dolor placeat. Consequatur,
-          modi, laborum, distinctio optio totam suscipit saepe deleniti vel
-          adipisci odit consequuntur officia quasi? Autem deserunt quas,
-          doloremque dignissimos molestiae ipsum officia voluptatum ullam error
-          ratione placeat delectus natus rerum explicabo.
-        </p>
+          <div className='skill-main-heading'>Front-End</div>
+          <div className='skill-text'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            nobis est odit esse sed numquam enim. Sint odio consectetur eligendi
+            suscipit delectus! Magnam, fugiat et labore explicabo dignissimos
+            nam modi corrupti, minima ipsum amet ab perspiciatis perferendis!
+            Natus, alias voluptatibus repudiandae ut aliquam maiores delectus
+            quae fugit voluptates cupiditate atque?
+          </div>
+          <div className='skill-subheading'>SUBHEADING</div>
+        </div>
+        {/* SKILL 3 */}
+        <div className='skill skill-3'>
+          <div className='skill-icon'>
+            <i className='fas fa-server' />
+          </div>
+          <div className='skill-main-heading'>Back-End</div>
+          <div className='skill-text'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+            tempore dignissimos pariatur, eos accusantium molestias magni
+            doloribus ducimus! Quo, eius?
+          </div>
+          <div className='skill-subheading'>SUBHEADING</div>
+        </div>
       </div>
     );
   }
