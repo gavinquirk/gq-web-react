38aba49d5b495f935431f37a7c3829f42da17d06
Gavin Quirk <gavinquirk@gmail.com>
2019-04-16 19:37:42 -0700

    animated scrolling for nav

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import { Link } from 'react-scroll';
+
 import './NavigationItem.css';
 
+// export default function NavigationItem(props) {
+//   return <li className='NavigationItem'>{props.children}</li>;
+// }
+
 export default function NavigationItem(props) {
-  return <li className='NavigationItem'>{props.children}</li>;
+  return (
+    <li className='NavigationItem'>
+      <Link
+        to={props.to}
+        activeClass='active'
+        spy={true}
+        smooth={true}
+        offset={0}
+        duration={500}
+      >
+        <span>{props.children}</span>
+      </Link>
+    </li>
+  );
 }
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,11 +7,11 @@ import './NavigationItems.css';
 export default function NavigationItems() {
   return (
     <ul className='NavigationItems'>
-      <NavigationItem section='Home'>Top</NavigationItem>
-      <NavigationItem section='SectionB'>About</NavigationItem>
-      <NavigationItem section='SectionC'>Projects</NavigationItem>
-      <NavigationItem section='SectionC'>Testimonials</NavigationItem>
-      <NavigationItem section='SectionC'>Contact</NavigationItem>
+      {/* <NavigationItem to='Lander'>Top</NavigationItem> */}
+      <NavigationItem to='Introduction'>About</NavigationItem>
+      <NavigationItem to='Portfolio'>Projects</NavigationItem>
+      <NavigationItem to='Testimonials'>Testimonials</NavigationItem>
+      <NavigationItem to='Contact'>Contact</NavigationItem>
     </ul>
   );
 }
